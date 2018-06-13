4dcae3a411ca521166a1003b7c9ce0e279bcf28b
Rafal Swieton <rafal@eskrzynka.pl>
2018-06-13 19:43:36 +0200

    Add and change folders, files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import PlayersList from './components/PlayersList/PlayersList';
+import AddPlayer from './components/AddPlayer/AddPlayer';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+ constructor() {
+   super();
+
+   this.state = {
+     players: []
+   }
+ }
+
+ onScoreUpdate = (playerIndex, scoreChange) => {
+  this.setState({
+    players: this.state.players.map((player, index) => {
+      if (index === playerIndex) {
+        return { ...player, score: player.score + scoreChange };
+      }
+      return player;
+    })
+  })
+ }
+
+ onPlayerAdd = (playerName) => {
+  const newPlayer = {
+    name: playerName,
+    score: 0,
   }
+  this.setState({
+    players: [...this.state.players, newPlayer]
+  })
+ }
+
+ render() {
+   return (
+     <div className="App">
+       <AddPlayer onPlayerAdd={this.onPlayerAdd} />
+       <PlayersList players={this.state.players} onScoreUpdate={this.onScoreUpdate}/>
+     </div>
+   );
+ }
 }
 
 export default App;
